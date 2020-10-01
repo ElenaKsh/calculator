c911fff89b0c6bbc7418841fb3bac1a3364a448f
Elena <olimpiada100@yandex.ru>
2020-10-01 21:58:29 +0300

    negative

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function count(number1, number2, action) {
-  switch (action) {
+  switch(action) {
     case "+":
       return Number(number1) + Number(number2);
     case "-":
@@ -7,7 +7,7 @@ function count(number1, number2, action) {
     case "*":
       return Number(number1) * Number(number2);
     case "/":
-      if (number2 !== 0) {
+      if(number2 !== 0){
         return Number(number1) / Number(number2);
       } else {
         return "На 0 не делится"
@@ -16,13 +16,11 @@ function count(number1, number2, action) {
 }
 
 function calculator(newString) {
-  if (typeof newString !== "string") {
+  if(typeof newString !== "string") {
   return "Введена не строка";
   }
-  let numbers = newString.split(/\+|-|\*|\//g, 5);
-  let signs = newString.split(/[0-9]+/g).filter(String);
-  console.log(numbers);
-  console.log(signs);
+  let signs = newString.match(/\+|-(?!\d)|\*|\/|(?<!\()-/g);
+  let numbers = newString.match(/\d+|(?<=\()\-?\d+(?=\))/g);
   if(signs == '' || numbers == ''){
     return "Ничего не введено";
   }
@@ -30,22 +28,33 @@ function calculator(newString) {
     return "Введенно более пяти слагаемых";
   }
 
-  for (i=0; signs[i] == "*" || signs[i] == "/"; i++){
-    let expression = count(numbers[i], numbers[i+1], signs[i]);
-    if (expression == Infinity) {
-      return "На 0 не делится"
-    }
-    signs.splice(i,1);
-    numbers[i] = expression;
-    numbers.splice(i+1,1);
-    i--;
-  }
-  for(i = 0; signs[i] == "+" || signs[i] == "-"; i++){
-      let expression = count(numbers[i],numbers[i+1],signs[i]);
+  for(i=0; signs.indexOf("*") != -1 || signs.indexOf("/") != -1; i++){
+
+
+    if(signs[i] == "*" || signs[i] == "/"){
+      let expression = count(numbers[i], numbers[i+1], signs[i]);
+      if (expression == Infinity) {
+        return "На 0 не делится"
+      }
+
+
       signs.splice(i,1);
       numbers[i] = expression;
       numbers.splice(i+1,1);
       i--;
-}
+    }
+
+
+  }
+  for(i = 0; signs.indexOf("+") != -1 || signs.indexOf("-") != -1;){
+
+
+    let expression = count(numbers[i],numbers[i+1],signs[i]);
+    signs.splice(i,1);
+    numbers[i] = expression;
+    numbers.splice(i+1,1);
+
+    
+  }
   return isNaN(numbers[0]) ? 'Введено более пяти слагаемых' : numbers[0];
 }
